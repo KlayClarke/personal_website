9cd121d98bd7c7feca0a042eee1705f88c68763a
Klay Clarke <clarkeklay@gmail.com>
2022-04-25 16:13:15 -0400

    add lorem text for skeleton of experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,45 @@
 export default function Experience() {
-  return <div name="experience" className="w-full h-screen border-b-2"></div>;
+  return (
+    <div name="experience" className="w-full h-screen">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-4">
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline">Experience</p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full px-4 grid lg:grid-cols-2 gap-4">
+          <div>
+            <p className="text-4xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga
+              facere dignissimos voluptatem laudantium labore dolorum,
+              perspiciatis obcaecati debitis vero minus suscipit ipsa porro
+              dolores, aut sint sunt dicta iure excepturi.
+            </p>
+          </div>
+          <div>
+            <p className="text-2xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi ut
+              quod, quos itaque temporibus reiciendis saepe, corporis eveniet
+              exercitationem vero rerum perferendis mollitia laudantium dolorum
+              sed animi, earum distinctio sint.
+            </p>
+            <br />
+            <p className="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              aliquid, sint inventore minima illum omnis, aperiam deleniti
+              accusamus dignissimos laboriosam temporibus et praesentium
+              explicabo doloribus soluta quis. Voluptas, reiciendis corrupti.
+            </p>
+            <br />
+            <p className="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui error
+              facere commodi modi harum odio cumque debitis nemo non vero a
+              porro, laudantium molestias animi! Ipsum qui molestiae rerum a.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
