e1a2a6e4b8ee2a22e65db9b8fe0cafe2fe6d6490
Klay Clarke <clarkeklay@gmail.com>
2022-04-25 12:57:57 -0400

    add about, portfolio and experience sections to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import About from "./components/About";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
+      <Portfolio />
+      <Experience />
     </div>
   );
 }
